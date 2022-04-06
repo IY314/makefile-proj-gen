c2fef090dea88ed1763ecc8dcbcd9ed21fa4ddbb
Isaac Yee <48425750+IY314@users.noreply.github.com>
2022-04-05 17:31:43 -0700

    Fix main function

--- src/main.c
+++ src/main.c
@@ -213,9 +213,16 @@ struct Project *getOptions(int argc, char **argv) {
 }
 
 int main(int argc, char **argv) {
-    struct Options *opts = getOptions(argc, argv);
+    struct Project *proj = getOptions(argc, argv);
 
-    free(opts);
+    int status = buildDirectory(proj);
+    if (status == 0) {
+        printf("mpg: created project '%s'\n", proj->name);
+    } else {
+        printf("mpg: failed to create project '%s'\n", proj->name);
+    }
+
+    freeProject(proj);
 
     return 0;
 }
